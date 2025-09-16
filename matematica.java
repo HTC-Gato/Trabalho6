925306a39c11f082eb044fce3e0344ea45960f31
HTC-Gato <heitort3ixeira@gmail.com>
2025-09-16 18:50:11 -0300

    Create matematica.java

---
+++ matematica.java
@@ -0,0 +1,21 @@
+package modelo;
+
+public class Matematica {
+	
+	public float somar(float v1, float v2) {
+		return v1 + v2;
+	}
+	
+	public float subtrair(float v1, float v2) {
+		return v1 - v2;
+	}
+	
+	public float multiplicar(float v1, float v2) {
+		return v1 * v2;
+	}
+	
+	public float dividir(float v1, float v2) {
+		return v1 / v2;
+	}
+
+}
